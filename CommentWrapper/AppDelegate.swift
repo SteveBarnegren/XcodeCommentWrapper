8a11f560ab7e94c2605ab629c8a3196cd4eaac7f
Steve Barnegren <steve.barnegren@gmail.com>
2018-04-06 22:48:34 +0100

    Initial Commit

---
+++ CommentWrapper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CommentWrapper
+//
+//  Created by Steve Barnegren on 06/04/2018.
+//  Copyright © 2018 Steve Barnegren. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CommentWrapper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CommentWrapper
+//
+//  Created by Steve Barnegren on 06/04/2018.
+//  Copyright © 2018 Steve Barnegren. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
