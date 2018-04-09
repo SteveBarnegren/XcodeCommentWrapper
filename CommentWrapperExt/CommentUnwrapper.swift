c51fbbde672c5a875052e29cd4801f229da8e2c4
Steve Barnegren <steve.barnegren@gmail.com>
2018-04-09 21:20:41 +0100

    Unwrapping handles newlines

--- CommentWrapperExt/CommentUnwrapper.swift
+++ CommentWrapperExt/CommentUnwrapper.swift
@@ -15,27 +15,62 @@ class CommentUnwrapper {
         let prefix = string.commentPrefix()
         let unprefixedString = string.lines()
             .map { $0.removing(prefix: prefix) }
+            .map { $0.removing(prefix: prefix.trimmingTrailingWhitespace()) }
             .joined(separator: "\n")
                 
         let items = Itemizer.itemize(string: unprefixedString)
-        return prefix + unwrappedString(fromItems: items)
+        return unwrappedString(fromItems: items)
+            .lines()
+            .map {
+                if ($0.isEmpty == false) {
+                    return prefix + $0
+                } else {
+                    return $0
+                }
+            }
+            .joined(separator: "\n")
     }
     
     private func unwrappedString(fromItems items: [StringItem]) -> String {
         
         var unwrappedString = String()
         
+        var lastItem: StringItem?
+        
+        var spacePending = false
+        func commitPendingSpace() {
+            if spacePending == true {
+                unwrappedString += " "
+                spacePending = false
+            }
+        }
+        
         var reversed = Array(items.reversed())
         while let next = reversed.popLast() {
             
+            //next.debug_print()
+            
             switch next {
             case .word(let word):
+                commitPendingSpace()
                 unwrappedString += word
             case .space:
-                unwrappedString += " "
+                commitPendingSpace()
+                spacePending = true
             case .newline:
-                unwrappedString += " "
+                if let lastItem = lastItem, case .newline = lastItem {
+                    if spacePending {
+                        unwrappedString += "\n"
+                        spacePending = false
+                    }
+                    
+                    unwrappedString += "\n"
+                } else {
+                    spacePending = true
+                }
             }
+            
+            lastItem = next
         }
         
         return unwrappedString
--- CommentWrapperTests/CommentUnwrapperTests.swift
+++ CommentWrapperTests/CommentUnwrapperTests.swift
@@ -75,4 +75,30 @@ class CommentUnwrapperTests: XCTestCase {
         
         XCTAssertEqual(output, expected)
     }
+    
+    // MARK: - Newlines
+    
+    func testHandlesNewlines() {
+        
+        ////////////////////////////////////////*** <-- Guide (40 chars + 3 prefix)
+        let input = """
+        // ant bear cat dog emu fox gecko heron
+        // iguana jellyfish koala lion monkey newt
+        // octopus parrot quail rabbit sheep tiger
+        // uakari vole walrus xenopus yak zebra
+        //
+        // ant bear cat dog emu fox gecko heron
+        // iguana jellyfish koala lion monkey newt
+        // octopus parrot quail rabbit sheep tiger
+        // uakari vole walrus xenopus yak zebra
+        """
+        
+        let expected = "// " + TestStrings.alphabeticalAnimals + "\n\n" + "// " + TestStrings.alphabeticalAnimals
+        
+        
+        let commentUnwrapper = CommentUnwrapper()
+        let output = commentUnwrapper.unwrap(string: input)
+        
+        XCTAssertEqual(output, expected)
+    }
 }
