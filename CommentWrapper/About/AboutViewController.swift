01b096e98f0b47df669c0a76efc579a56c3aa08a
Steve Barnegren <steve.barnegren@gmail.com>
2018-04-21 16:48:02 +0100

    Added about section

--- CommentWrapper/About/AboutViewController.swift
+++ CommentWrapper/About/AboutViewController.swift
@@ -7,12 +7,57 @@
 //
 
 import Cocoa
+import AttributedStringBuilder
 
 class AboutViewController: NSViewController {
+    
+    // MARK: - Views
+    
+    @IBOutlet private var websiteButton: NSButton!
+    @IBOutlet private var twitterButton: NSButton!
+    @IBOutlet private var githubButton: NSButton!
 
+    // MARK: - UIViewController
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do view setup here.
+        styleButtons()
     }
     
+    func styleButtons() {
+        
+        [websiteButton, twitterButton, githubButton].forEach {
+            
+            let text = $0!.title
+            let font = $0!.font!
+            let color = NSColor.blue
+            
+            let attributedString =
+                AttributedStringBuilder()
+                    .text(text, attributes: [.font(font), .textColor(color)])
+                    .attributedString
+            
+            $0!.attributedTitle = attributedString
+        }
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction private func websiteButtonPressed(sender: NSButton) {
+        if let url = URL(string: "https://www.stevebarnegren.com") {
+            NSWorkspace.shared.open(url)
+        }
+    }
+    
+    @IBAction private func twitterButtonPressed(sender: NSButton) {
+        if let url = URL(string: "https://www.twitter.com/stevebarnegren") {
+            NSWorkspace.shared.open(url)
+        }
+    }
+    
+    @IBAction private func githubButtonPressed(sender: NSButton) {
+        if let url = URL(string: "https://github.com/SteveBarnegren/XcodeCommentWrapper") {
+            NSWorkspace.shared.open(url)
+        }
+    }
 }
