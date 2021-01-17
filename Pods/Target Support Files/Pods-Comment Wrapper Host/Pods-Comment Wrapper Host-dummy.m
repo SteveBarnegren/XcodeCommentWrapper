eea604f25f1885ccdace6ba496cec49b1ebbbb95
Steve Barnegren <steve.barnegren@gmail.com>
2021-01-17 10:49:46 +0000

    Place cocoapods with spm

--- Pods/Target Support Files/Pods-Comment Wrapper Host/Pods-Comment Wrapper Host-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Comment_Wrapper_Host : NSObject
-@end
-@implementation PodsDummy_Pods_Comment_Wrapper_Host
-@end
--- Pods/Target Support Files/Pods-Comment Wrapper/Pods-Comment Wrapper-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Comment_Wrapper : NSObject
-@end
-@implementation PodsDummy_Pods_Comment_Wrapper
-@end
